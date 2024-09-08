e2dc7bd80a2c5bb9ca4ddd1db7d589067e2ed098
OranJun <Cheng_Junhong_2023@sjtu.edu.cn>
2024-09-08 10:41:43 +0800

    finish getDomSet

--- src/main/java/juhuh/compiler/opt/mem2reg/domBuilder.java
+++ src/main/java/juhuh/compiler/opt/mem2reg/domBuilder.java
@@ -2,7 +2,6 @@ package juhuh.compiler.opt.mem2reg;
 
 import java.util.HashMap;
 
-import juhuh.compiler.frontend.irVisitor;
 import juhuh.compiler.ir.*;
 import juhuh.compiler.ir.def.*;
 import juhuh.compiler.ir.ins.*;
@@ -10,78 +9,119 @@ import juhuh.compiler.ir.stmt.irBlock;
 import juhuh.compiler.util.vector;
 import juhuh.compiler.util.error.error;
 
-import java.util.List;
+import java.util.BitSet;
+import java.util.Collections;
 
-public class domBuilder{
+public class domBuilder {
   private HashMap<Integer, dom> doms = new HashMap<>();
   private HashMap<String, Integer> id = new HashMap<>();
   private vector<irBlock> id2B;
   private int cnt = 0;
   private vector<Integer>[] preds, dom;
   private vector<Integer> postRev;
+  BitSet[] domFlag = new BitSet[cnt];
   public void visit(irRoot node) throws error {
-    for(var def : node.getFDef()) {
+    for (var def : node.getFDef()) {
       visit(def);
     }
   }
+
   private void initBlock(irBlock block) {
     // consider block.name == "entry", it will be put at first
     id.put(block.getLabel(), cnt);
     id2B.add(block);
     ++cnt;
   }
+
   private boolean visitBlock(int idx) {
-    if(doms.containsKey(idx) && idx != 0) {
+    if (doms.containsKey(idx) && idx != 0) {
       return false;
     }
     doms.put(idx,
-      new dom(null, null));
+        new dom(null, null));
     return true;
   }
+
   private void initFunc(irFuncDef node) {
     // init doms
     cnt = 0;
     initBlock(node.getEntry());
-    for(var block : node.getBody()) {
+    for (var block : node.getBody()) {
       initBlock(block);
     }
     initBlock(node.getRet());
   }
-  private void addPred(irBlock block) {
-    // addpred block.label <- block.jump.dest
 
-  }
   @SuppressWarnings("unchecked")
   private void initPreds(irFuncDef node) {
     preds = new vector[cnt];
     dom = new vector[cnt];
-    for(int i = 0; i < cnt; ++i) {
+    for (int i = 0; i < cnt; ++i) {
       preds[i] = new vector<>();
       dom[i] = new vector<>();
     }
   }
+  private void getDom() {
+    BitSet tmp = new BitSet(cnt);
+    for (int i = 0; i < cnt; ++i) {
+      domFlag[i] = new BitSet(cnt);
+      domFlag[i].set(0, cnt);
+    }
+    domFlag[0].set(0, true);
+    boolean changed = true;
+    while(changed) {
+      changed = false;
+      for (int i = 1; i < cnt; ++i) {
+        tmp.clear();
+        tmp.set(0, cnt);
+        for (var pred : preds[i]) {
+          tmp.and(domFlag[pred]);
+        }
+        tmp.set(i, true);
+        if (!tmp.equals(domFlag[i])) {
+          domFlag[i] = (BitSet)tmp.clone();
+          changed = true;
+        }
+      }
+    }
+    for (int i = 1; i < cnt; ++i) {
+      for (int j = 1; j < cnt; ++j) {
+        if (domFlag[i].get(j)) {
+          dom[i].add(j);
+        }
+      }
+    }
+    
+  }
   private void Visit(int idS, irBlock block) {
     // postReverseOrder
     irIns endT;
-    if(block.getTerminalstmt() != null) {
+    if (block.getTerminalstmt() != null) {
       endT = block.getTerminalstmt();
     } else {
       endT = block.getEndTerm();
     }
-    if(endT instanceof irJump) {
+    if (endT instanceof irJump) {
       // label -> block
-      var idx = id.get(((irJump)endT).getDest());
+      var idx = id.get(((irJump) endT).getDest());
       preds[idx].add(idS);
-      if(visitBlock(idx))
+      if (visitBlock(idx))
         Visit(idx, id2B.get(idx));
-      
-    } else if(endT instanceof irBranch) {
-      
+    } else if (endT instanceof irBranch) {
+      var idx = id.get(((irBranch) endT).iftrue);
+      preds[idx].add(idS);
+      if (visitBlock(idx))
+        Visit(idx, id2B.get(idx));
+      idx = id.get(((irBranch) endT).iffalse);
+      preds[idx].add(idS);
+      if (visitBlock(idx))
+        Visit(idx, id2B.get(idx));
     } else {
       throw new error("invalid terminal statement");
     }
     postRev.add(idS);
   }
+
   public void visit(irFuncDef node) throws error {
     // cnt only acccesible in this function
     initFunc(node);
@@ -89,6 +129,9 @@ public class domBuilder{
     initPreds(node);
     visitBlock(0);
     Visit(0, node.getEntry());
-
+    // build dominator tree using postReverseOrder
+    // reverse postRev
+    Collections.reverse(postRev);
+    getDom();
   }
 }
