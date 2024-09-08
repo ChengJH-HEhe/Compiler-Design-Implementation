f8db50c46f3c7bda9470a02f79ded397955670b3
OranJun <Cheng_Junhong_2023@sjtu.edu.cn>
2024-09-09 05:01:42 +0800

    finish replace values

--- src/main/java/juhuh/compiler/opt/mem2reg/domBuilder.java
+++ src/main/java/juhuh/compiler/opt/mem2reg/domBuilder.java
@@ -3,6 +3,7 @@ package juhuh.compiler.opt.mem2reg;
 import java.util.HashMap;
 import java.util.Stack;
 
+import juhuh.compiler.frontend.irVisitor;
 import juhuh.compiler.ir.*;
 import juhuh.compiler.ir.def.*;
 import juhuh.compiler.ir.ins.*;
@@ -13,7 +14,7 @@ import juhuh.compiler.util.error.error;
 import java.util.BitSet;
 import java.util.Collections;
 
-public class domBuilder {
+public class domBuilder implements irVisitor {
   private vector<dom> doms;
   private HashMap<String, Integer> id = new HashMap<>();
   private vector<irBlock> id2B;
@@ -157,23 +158,44 @@ public class domBuilder {
   }
 
   vector<irBlock> path;
+
   boolean isTmpName(String name) {
     return name.length() >= 2 && name.charAt(0) == '%' && name.charAt(1) == '_';
   }
-
+  HashMap<String, String> curName;
   private void placePhi(irBlock block) {
     // entry block has alloca need to neglect
-    // this' lst-def -> domF's prePhi 
+    // this' lst-def -> domF's prePhi
     path.add(block);
-    // upd block's firstload reg->ptr
-    for(var stmt : block.getStmts()) {
-       
+    curName = new HashMap<>();
+    // upd thisblock's firstload
+    // reg->ptr
+    var vecStmt = block.getStmts();
+    // replace vecStmt
+    vector<irIns> vec = new vector<irIns>();
+    if (vecStmt == null)
+      return;
+    for (int i = 0; i < vecStmt.size(); ++i) {
+      var stmt = (irIns) (vecStmt.get(i));
+      if ((stmt instanceof irLoad && isTmpName(((irLoad) stmt).getPtr())) ||
+          (stmt instanceof irStore && isTmpName(((irStore) stmt).getPtr()))) {
+        ++i;
+        continue;
+      }
+      // distinguish from phi or from lastBlock?
+      // stmt's val
+      stmt.accept(this);
+      vec.add(stmt);
+      // if from phi then replace it by thisBlock's phi
+      // if from lastBlock then replace it by lastBlock's lastDef
     }
-    // this Block's phi ptr->phiIns map
+    // TODO: this Block's phi ptr->phiIns map
+    // upd block's lastdef withPHI
 
-    // upd block's lastdef
+    // TODO: upd domF's Phi
   }
 
+  @Override
   public void visit(irFuncDef node) throws error {
     // cnt only acccesible in this function
     initFunc(node);
@@ -188,5 +210,116 @@ public class domBuilder {
     path = new vector<>();
     // place PHI cmd
     placePhi(node.getEntry());
+  }
+
+  @Override
+  public void visit(irNode node) throws error {
+    node.accept(this);
+  }
+
+  @Override
+  public void visit(irGlobalDef node) throws error {
+    // do nothing
+  }
+
+  @Override
+  public void visit(irStrDef node) throws error {
+    // do nothing
+  }
+  private String findPre(String Ptr) throws error{
+    for(int i = path.size() - 2; i >= 0; --i) {
+      var res = path.get(i).findVal(Ptr);
+      if(res != null)
+        return res;
+    }
+    throw new error("**** there is no pre-Def !!!!");
+  }
+  private String replace(irBlock block, String oldReg) {
+    if(curName.containsKey(oldReg))
+      return curName.get(oldReg);
+    var res = block.findFirstLoad(oldReg);
+    String ans = "";
+    if (res != null) {
+      if(block.getPhi().containsKey(res))
+        ans = res + "." + block.getLabel();
+      else {
+        ans = findPre(res); 
+      }
+    } else ans = oldReg;    
+    curName.put(oldReg, ans);
+    return ans;
+  }
+
+  @Override
+  public void visit(irBinary node) throws error {
+    var block = path.getlst();
+    node.setOp1(replace(block, node.getOp1()));
+    node.setOp2(replace(block, node.getOp2()));
+  }
+
+  @Override
+  public void visit(irIcmp node) throws error {
+    var block = path.getlst();
+    node.setOp1(replace(block, node.getOp1()));
+    node.setOp2(replace(block, node.getOp2()));
+  }
+
+  @Override
+  public void visit(irSelect node) throws error {
+    var block = path.getlst();
+    node.setVal1(replace(block, node.getVal1()));
+    node.setVal2(replace(block, node.getVal2()));
+  }
+
+  @Override
+  public void visit(irBranch node) throws error {
+    // do nothing
+  }
+
+  @Override
+  public void visit(irJump node) throws error {
+    // do nothing
+  }
+
+  @Override
+  public void visit(irRet node) throws error {
+    // do nothing
+  }
+
+  @Override
+  public void visit(irCall node) throws error {
+    // replace node.val
+    var block = path.getlst();
+    for (int i = 0; i < node.getVal().size(); ++i) {
+      node.getVal().set(i, replace(block, node.getVal().get(i)));
+    }
+  }
+
+  @Override
+  public void visit(irAlloca node) throws error {
+    // do nothing
+  }
+
+  @Override
+  public void visit(irGetElement node) throws error {
+    //
+    var block = path.getlst();
+    node.setPtrval(replace(block, node.getPtrval()));
+  }
+
+  @Override
+  public void visit(irLoad node) throws error {
+    // done previously
+  }
+
+  @Override
+  public void visit(irStore node) throws error {
+    // getelementptr's result should be added too
+    // but if not
+  }
+
+  @Override
+  public void visit(irBlock node) throws error {
+    // shouldn't be visited
   }
 }
